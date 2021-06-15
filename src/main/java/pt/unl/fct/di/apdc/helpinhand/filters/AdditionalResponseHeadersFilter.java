7f648c364f97d998223c475ac40bd4f77800310e
davvaz <d.vaz@campus.fct.unl.pt>
2021-06-15 15:17:54 +0100

    changes in headers filter

--- src/main/java/pt/unl/fct/di/apdc/helpinhand/filters/AdditionalResponseHeadersFilter.java
+++ src/main/java/pt/unl/fct/di/apdc/helpinhand/filters/AdditionalResponseHeadersFilter.java
@@ -17,7 +17,8 @@ public class AdditionalResponseHeadersFilter implements ContainerResponseFilter 
 			throws IOException {
 				responseContext.getHeaders().add("Access-Control-Allow-Methods", "HEAD,GET,PUT,POST,DELETE,OPTIONS,PATCH");
 				responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-				responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+				responseContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, X-Requested-With");
+				requestContext.getHeaders().add("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, X-Requested-With");
 				requestContext.getHeaders().add("Access-Control-Allow-Origin", "*");
 	}
 	
